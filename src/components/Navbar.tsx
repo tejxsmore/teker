aea8658a9be88c3a5bc6bdcbced9d826272d18cc
tejxsmore <tejasmore1104@gmail.com>
2025-03-29 22:43:54 +0530

    Issues resolved

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,6 +15,7 @@ import {
 import { Button } from "@/components/ui/button"
 import ThemeToggle from "@/components/ThemeToggle"
 import { currentUser } from '@clerk/nextjs/server'
+import Link from 'next/link'
 
 import { ShoppingCart } from 'lucide-react';
 import { User } from 'lucide-react';
@@ -33,15 +34,18 @@ export default async function Navbar(){
             <ThemeToggle />
 
             <Button className="rounded-full bg-light text-dark border border-[#D8D9CF] 
-            hover:bg-light dark:border-[#404258]">
+            hover:bg-light dark:border-[#404258] cursor-pointer">
                 <span><ShoppingCart size={10} /></span>
                 <p>Cart</p>
             </Button>
 
-            <div>
+            <div className='flex items-center'>
                 <SignedOut>
-                    <SignInButton className="py-[7px] px-5 border border-[#D8D9CF]
-                    dark:border-[#404258] rounded-full cursor-pointer text-sm font-medium">Login</SignInButton>
+                    <div className="py-[7px] px-5 border border-[#D8D9CF] flex items-center gap-2
+                        dark:border-[#404258] rounded-full cursor-pointer text-sm font-medium">
+                        <span><User size={16} /></span>
+                        <SignInButton>Login</SignInButton>
+                    </div>
                 </SignedOut>
 
                 <SignedIn>
@@ -49,7 +53,7 @@ export default async function Navbar(){
                         <DropdownMenuTrigger className='focus:outline-none'>
                             <Button className="rounded-full bg-light text-dark border border-[#D8D9CF] 
                             hover:bg-light dark:border-[#404258] cursor-pointer">
-                                <span><User size={10} /></span>
+                                <span><User size={15} /></span>
                                 <p>Profile</p>
                             </Button>
                         </DropdownMenuTrigger>
@@ -62,13 +66,8 @@ export default async function Navbar(){
                                 {userEmail}
                             </DropdownMenuLabel>
 
-                            <DropdownMenuItem className='pt-[8px]'>Settings</DropdownMenuItem>
-                            <DropdownMenuItem className='pb-[9px]'>Help and Support</DropdownMenuItem>
-
-                            <SignOutButton className='p-2 pt-2.5 text-start text-sm border-t border-[#D8D9CF] 
-                            dark:border-[#404258] tracking-wider w-full cursor-pointer'>
-                                Logout
-                            </SignOutButton>
+                            <DropdownMenuItem className='pt-[8px] rounded-lg'>Settings</DropdownMenuItem>
+                            <DropdownMenuItem className=''>Help and Support</DropdownMenuItem>                            
                         </DropdownMenuContent>
                     </DropdownMenu>
                 </SignedIn>
