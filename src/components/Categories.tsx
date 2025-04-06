6217d29a706a938b705a4278964254b4f634e9ef
tejxsmore <tejasmore1104@gmail.com>
2025-04-06 21:36:22 +0530

    Issues fixed

--- src/components/Categories.tsx
+++
@@ -1,8 +0,0 @@
-import Link from 'next/link';
-import { Smartphone, Laptop, Headphones, Speaker, Camera, Gamepad2, Watch } from 'lucide-react';
-
-export default function Categories(){
-    return <div className="p-5 flex gap-5 overflow-x-auto overflow-y-hidden">
-        Categories
-    </div>
-}
